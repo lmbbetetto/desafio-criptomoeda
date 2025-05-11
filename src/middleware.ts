13ba2b3fe429df7e347ee0196d0b686a87cd2b41
lmbbetetto <lmbbetetto@gmail.com>
2025-05-11 17:51:11 -0300

    Fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -48,5 +48,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  atcher: ["/", "/api/conversion", "/api/favorites"],
+  matcher: ["/", "/api/conversion", "/api/favorites"],
 };
